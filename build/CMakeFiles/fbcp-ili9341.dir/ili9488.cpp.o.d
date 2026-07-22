07079c77d8840e42fa3ee0546e464177d2a8a13e
amriteshkr8 <amritesh.kr8@gmail.com>
2026-07-22 12:49:48 +0100

    cleanup, should detect rpi model automatically (untested), hits a max of 40-50 fps with "cmatrix -u 0", still uses 1.5 cores at max fps

--- build/CMakeFiles/fbcp-ili9341.dir/ili9488.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/fbcp-ili9341.dir/ili9488.cpp.o: \
- /home/ak/fbcp-ili9341-aarch64/ili9488.cpp \
- /usr/arm-linux-gnueabihf/include/stdc-predef.h \
- /home/ak/fbcp-ili9341-aarch64/config.h
--- build/CMakeFiles/fbcp-ili9341.dir/mz61581.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/fbcp-ili9341.dir/mz61581.cpp.o: \
- /home/ak/fbcp-ili9341-aarch64/mz61581.cpp \
- /usr/arm-linux-gnueabihf/include/stdc-predef.h \
- /home/ak/fbcp-ili9341-aarch64/config.h
