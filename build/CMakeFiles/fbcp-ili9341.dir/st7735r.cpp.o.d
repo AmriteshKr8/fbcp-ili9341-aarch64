8c9b814068addef88d81cf57c0f3f6f91e0af37f
amriteshkr8 <amritesh.kr8@gmail.com>
2026-07-23 07:58:34 +0100

    minor fixes

--- build/CMakeFiles/fbcp-ili9341.dir/st7735r.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/fbcp-ili9341.dir/st7735r.cpp.o: \
- /home/ak/fbcp-ili9341-aarch64/st7735r.cpp \
- /usr/arm-linux-gnueabihf/include/stdc-predef.h \
- /home/ak/fbcp-ili9341-aarch64/config.h
